244e813c8614ba4ae66a1d36474c5dc3a7317775
utsabk <kharelutsab@gmail.com>
2021-03-17 13:09:06 +0200

    User register - Endpoint to take register request - User added to DB - Pass on to login function - Login is handled as Promise - Logout to erase login session

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,26 +1,58 @@
 'use strict';
 import jwt from 'jsonwebtoken';
 import passport from '../utils/pass.js';
+import { uploadUserData } from '../models/userModel.js';
 
-const userAuth = (req, res, next) => {
-  passport.authenticate('local', { session: false }, (err, user, info) => {
-
-
-    if (err || !user) {
-      return res.status(400).json({
-        message: info.message,
-        user: user,
-      });
-    }
-    req.login(user, { session: false }, (err) => {
-      if (err) {
-        res.send(err);
+const authenticate = (req, res) => {
+  // Authentication handled as Promise
+  return new Promise((resolve, reject) => {
+    passport.authenticate('local', { session: false }, (err, user, info) => {
+      try {
+        if (err || !user) {
+          reject(info.message);
+        }
+        req.login(user, { session: false }, (err) => {
+          if (err) {
+            reject(err);
+          }
+          const token = jwt.sign(user, process.env.SECRET_KEY);
+          resolve({ user, token });
+        });
+      } catch (err) {
+        reject(err.message);
       }
-
-      const token = jwt.sign(user, process.env.SECRET_KEY);
-      return res.json({ user, token });
-    });
-  })(req, res);
+    })(req, res);
+  });
 };
 
-export { userAuth }
+const userLogin = async (req, res) => {
+  try {
+    const response = await authenticate(req, res);
+    console.log('response from authenticate', response);
+    return res.send({
+      id: response.user._id,
+      ...response.user,
+      token: response.token,
+    });
+  } catch (err) {
+    res.send({ error: err });
+    //throw new Error(err);
+  }
+};
+const userRegister = async (req, res, next) => {
+  const params = [req.body.username, req.body.email, req.body.password];
+  if (await uploadUserData(params)) {
+    // This will call next middleware on the list
+    next();
+  } else {
+    return res.status(400).json({ error: 'register error' });
+  }
+};
+
+const userLogout = async (req, res) => {
+  // Invoking logout() will remove the req.user property and clear the login session (if any).
+  req.logout(); 
+  res.json({message: 'logout'});
+}
+
+export { userLogin, userRegister, userLogout };
