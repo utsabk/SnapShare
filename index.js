a1de58fda4987f1f51a0ac991c62b1e1eaafb499
utsabk <kharelutsab@gmail.com>
2021-03-21 21:22:29 +0200

    Home page upgraded - DB model for image updated - comments & likes count added to image - Home page upgraded - PUG files changed to HTML - Populated with hard coded user data

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ import authRoutes from './routes/authRoutes.js';
 import commentRoutes from './routes/commentRoutes.js';
 const app = express();
 
-app.set('view engine', 'pug');
-app.set('views', './views'); //explicitly set the directory where templates are stored
-
 app.use(express.static('public'));
 app.use('/modules', express.static('node_modules'));
 app.use(express.json());
@@ -18,17 +15,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/image', imageRoutes);
 app.use('/auth', authRoutes);
 app.use('/comment', commentRoutes);
-
-app.get('/', (req, res) => res.render('signin'));
-app.get('/signup', (req, res) => res.render('signup'));
-app.get('/signin', (req, res) => res.render('signin'));
-app.get('/upload', (req, res) => res.render('upload'));
-
-// app.get('*', (req, res) => {
-//   console.log(req.pathname);
-//   const url = req.url.split('/')
-//   res.render(url[1])
-// })
 
 app.listen(process.env.APP_PORT, () => {
   console.log(`App running on port ${process.env.APP_PORT}`);
